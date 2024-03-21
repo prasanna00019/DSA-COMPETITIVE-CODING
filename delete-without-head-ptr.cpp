832d441520d1377252a6bcedabb84776ab5842af
prasanna00019 <159623027+prasanna00019@users.noreply.github.com>
2024-03-21 22:46:54 +0530

    Create delete-without-head-ptr.cpp

---
+++ delete-without-head-ptr.cpp
@@ -0,0 +1,29 @@
+/*
+Delete without head pointer
+
+You are given a node del_node of a Singly Linked List where you have to delete a 
+value of the given node from the linked list but you are not given the head of the list.
+By deleting the node value, we do not mean removing it from memory. We mean:
+
+The value of the given node should not exist in the linked list.
+The number of nodes in the linked list should decrease by one.
+All the values before & after the del_node node should be in the same order.
+Note:
+
+Multiple nodes can have the same values as the del_node, but you must only remove the node whose pointer del_node is given to you.
+It is guaranteed that there exists a node with a value equal to the del_node value and it will not be the last node of the linked list.
+*/
+
+ void swap(int&a ,int &b){
+        int temp=a;
+        a=b;
+        b=temp;
+    }
+    void deleteNode(Node *del_node)
+    {
+       // Your code. here
+     swap(del_node->data,del_node->next->data);
+     Node* n=del_node->next;
+     del_node->next=del_node->next->next;
+     free(n);
+    }
