ae2b4127040a1de76742ea1df4c18da6b8606a6a
prasanna00019 <159623027+prasanna00019@users.noreply.github.com>
2024-06-27 10:50:07 +0530

    Create Daily_Temperatures.cpp

---
+++ Daily_Temperatures.cpp
@@ -0,0 +1,17 @@
+// #LEETCODE
+vector<int> dailyTemperatures(vector<int>& temperatures) {
+         int n=temperatures.size();
+         vector<int>ans(n);
+         stack<int>st;
+         for(int i=n-1;i>=0;i--){
+            while(!st.empty() && temperatures[st.top()]<=temperatures[i]){
+                st.pop();
+            }
+            if(st.empty()){ans[i]=0;}
+            else{
+                ans[i]=st.top()-i;
+            }
+            st.push(i);
+         }
+         return ans;
+    }
