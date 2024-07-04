17a8087294e84cdde1fad61683a5e0128f8ad879
prasanna00019 <159623027+prasanna00019@users.noreply.github.com>
2024-07-04 21:36:55 +0530

    Create Duplicate_subtress.cpp

---
+++ Duplicate_subtress.cpp
@@ -0,0 +1,22 @@
+string helper(Node* root,vector<Node*>&ans,unordered_map<string,int>&mp){
+      string temp="";
+      if(root==NULL){
+          return "";
+      }
+      temp+=to_string(root->data);
+      temp+=helper(root->left,ans,mp);
+      temp+=helper(root->right,ans,mp);
+      if(mp[temp]==1){
+          ans.push_back(root);
+      }
+      mp[temp]++;
+            cout<<temp<<endl;
+
+      return temp;
+  }
+    vector<Node*> printAllDups(Node* root) {
+        vector<Node*>ans;
+        unordered_map<string,int>mp;
+        helper(root,ans,mp);
+        return ans;
+    }
