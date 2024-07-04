34de7d13e9ceed4ca2a4ec818a81b98d5a40901b
prasanna00019 <159623027+prasanna00019@users.noreply.github.com>
2024-07-04 22:00:55 +0530

    Update merge_node_in_between_zeros.cpp
    optimized

--- merge_node_in_between_zeros.cpp
+++ merge_node_in_between_zeros.cpp
@@ -1,15 +1,21 @@
 //leetcode
+   
 ListNode* mergeNodes(struct ListNode* head) {
- ListNode* temp1,*temp2;
- temp1=head;int c=0; temp2=NULL;
- while(temp1->next!=NULL){ 
- c=0;
-while(temp1->next->val!=0){
-  c+=temp1->next->val;
-  temp1=temp1->next;
+ ListNode* dummy=new ListNode(-1);
+ ListNode* t1=dummy;
+ ListNode* temp1=head;
+ int c;
+ while(temp1->next!=NULL){
+    c=0;
+    while(temp1->next->val!=0){
+        c+=temp1->next->val;
+        temp1=temp1->next;
+    }
+    t1->next=new ListNode(c);
+    t1=t1->next;
+    temp1=temp1->next;
+
  }
- temp1=temp1->next;
- temp2=insert_front(temp2,c);
+ return dummy->next;
 }
- return reverse(temp2);
-}
+   
